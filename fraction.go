c2f91064c31f6136f25192abf9c23a9381131720
yidane <yidane@163.com>
2019-03-22 01:49:38 +0800

    finish rep

--- fraction.go
+++ fraction.go
@@ -1,45 +1,71 @@
 package fraction
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 )
+
+var errDivideByZero = errors.New("fraction: attempt to divide by zero")
 
 type Fraction struct {
 	divisor  int64
 	dividend int64
 }
 
-func New(divisor, dividend int64) Fraction {
+func New(divisor, dividend int64) (Fraction, error) {
 	if dividend == 0 {
-		dividend = 1
-		divisor = 0
+		return Fraction{}, errDivideByZero
+	}
 
-		return Fraction{divisor: divisor, dividend: dividend}
+	if divisor == 0 {
+		return Fraction{divisor: 0, dividend: 1}, nil
+	}
+
+	return newFraction(divisor, dividend), nil
+}
+
+func newFraction(divisor, dividend int64) Fraction {
+	if dividend < 0 {
+		divisor = -divisor
+		dividend = -dividend
+	}
+
+	positive := true
+	if divisor < 0 {
+		positive = false
+		divisor = -divisor
 	}
 
 	maxDivisor := findMaxCommonDividend(divisor, dividend)
 
-	if maxDivisor == 1 {
-		return Fraction{divisor: divisor, dividend: dividend}
+	divisor = divisor / maxDivisor
+	dividend = dividend / maxDivisor
+
+	if !positive {
+		divisor = -divisor
 	}
 
-	return Fraction{divisor: divisor / maxDivisor, dividend: dividend / maxDivisor}
+	return Fraction{divisor: divisor, dividend: dividend}
 }
 
 func findMaxCommonDividend(i, j int64) int64 {
-	m := i % j
-	for m != 0 {
-		if j > m {
-			i, j = j, m
-		} else {
-			i = m
-		}
-
-		m = i % j
+	if i < 0 {
+		i = -i
 	}
 
-	return j
+	if j < 0 {
+		j = -j
+	}
+
+	if i < j {
+		i, j = j, i
+	}
+
+	for j != 0 {
+		i, j = j, i%j
+	}
+	return i
 }
 
 func (fraction Fraction) Add(other Fraction) Fraction {
@@ -49,44 +75,45 @@ func (fraction Fraction) Add(other Fraction) Fraction {
 	dividend := fTimes * oTimes * maxDivisor
 	divisor := fraction.divisor*oTimes + other.divisor*fTimes
 
-	return New(divisor, dividend)
+	return newFraction(divisor, dividend)
 }
 
 func (fraction Fraction) Subtract(other Fraction) Fraction {
 	if fraction.Equal(other) {
-		return New(0, 1)
+		return newFraction(0, 1)
 	}
 
 	maxDivisor := findMaxCommonDividend(fraction.dividend, other.dividend)
 	fTimes, oTimes := fraction.dividend/maxDivisor, other.dividend/maxDivisor
 
 	dividend := fTimes * oTimes * maxDivisor
-	divisor := (fraction.divisor*oTimes - other.divisor*fTimes) * maxDivisor
+	divisor := fraction.divisor*oTimes - other.divisor*fTimes
 
-	return New(divisor, dividend)
+	return newFraction(divisor, dividend)
 }
 
 func (fraction *Fraction) Multiply(other Fraction) Fraction {
 	maxDivisor1 := findMaxCommonDividend(fraction.divisor, other.dividend)
 	maxDivisor2 := findMaxCommonDividend(fraction.dividend, other.divisor)
 
-	divisor := fraction.divisor * (other.dividend / maxDivisor1)
-	dividend := fraction.dividend * (other.divisor / maxDivisor2)
+	divisor := (fraction.divisor / maxDivisor1) * (other.divisor / maxDivisor2)
+	dividend := (fraction.dividend / maxDivisor2) * (other.dividend / maxDivisor1)
 
-	return New(divisor, dividend)
+	return newFraction(divisor, dividend)
 }
 
-func (fraction Fraction) Divide(other Fraction) Fraction {
+func (fraction Fraction) Divide(other Fraction) (Fraction, error) {
 	if other.IsZero() {
-		panic("dividend is zero")
+		return Fraction{}, errDivideByZero
 	}
 
-	maxDivisor := findMaxCommonDividend(fraction.dividend, other.divisor)
+	maxDivisor1 := findMaxCommonDividend(fraction.divisor, other.divisor)
+	maxDivisor2 := findMaxCommonDividend(fraction.dividend, other.dividend)
 
-	dividend := fraction.dividend * (other.divisor / maxDivisor)
-	divisor := fraction.divisor * (other.divisor / maxDivisor)
+	divisor := (fraction.divisor / maxDivisor1) * (other.dividend / maxDivisor2)
+	dividend := (fraction.dividend / maxDivisor2) * (other.divisor / maxDivisor1)
 
-	return New(divisor, dividend)
+	return newFraction(divisor, dividend), nil
 }
 
 func (fraction Fraction) Greater(other Fraction) bool {
@@ -115,12 +142,12 @@ func (fraction Fraction) Float32() float32 {
 	return float32(fraction.divisor) / float32(fraction.dividend)
 }
 
-//func (fraction Fraction)Float64Size(s int)float32{
-//
-//}
-
 func (fraction Fraction) Float64() float64 {
 	return float64(fraction.divisor) / float64(fraction.dividend)
+}
+
+func (fraction Fraction) Positive() bool {
+	return fraction.divisor >= 0
 }
 
 func (fraction Fraction) Equal(other Fraction) bool {
--- fraction_test.go
+++ fraction_test.go
@@ -31,15 +31,17 @@ func TestNew(t *testing.T) {
 		{18, 3, "6"},
 		{18, 2, "9"},
 		{18, 1, "18"},
+		{-2, 4, "-1/2"},
+		{2, -4, "-1/2"},
+		{-2, -4, "1/2"},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%d/%d", tt.divisor, tt.dividend), func(t *testing.T) {
-			fraction := New(tt.divisor, tt.dividend)
+			fraction, _ := New(tt.divisor, tt.dividend)
 
-			fmt.Println(fraction)
-
-			if fraction.String() != tt.want {
-				t.Fatalf("%d/%d should be equal %s", tt.divisor, tt.dividend, tt.want)
+			fras := fraction.String()
+			if fras != tt.want {
+				t.Fatalf("%d/%d should be equal %s but %s", tt.divisor, tt.dividend, tt.want, fras)
 			}
 		})
 	}
@@ -50,14 +52,138 @@ func TestFraction_Add(t *testing.T) {
 		{1, 2, 3, 4, 5, 4},
 		{3, 7, 4, 7, 1, 1},
 		{3, 7, 4, 9, 55, 63},
+		{1, 2, -2, 4, 0, 1},
+		{1, 2, 2, -4, 0, 2},
 	}
 	for _, arr := range tests {
 		t.Run(fmt.Sprint(arr), func(t *testing.T) {
-			fraction := New(arr[0], arr[1])
-			other := New(arr[2], arr[3])
-			result := New(arr[4], arr[5])
+			fraction, _ := New(arr[0], arr[1])
+			other, _ := New(arr[2], arr[3])
+			result, _ := New(arr[4], arr[5])
 			if got := fraction.Add(other); !got.Equal(result) {
 				t.Errorf("Fraction.Add() = %v, want %v", got, result)
+			}
+		})
+	}
+}
+
+func BenchmarkFraction_Add(b *testing.B) {
+	tests := [][]int64{
+		{1, 2, 3, 4, 5, 4},
+		{3, 7, 4, 7, 1, 1},
+		{3, 7, 4, 9, 55, 63},
+		{1, 2, -2, 4, 0, 1},
+		{1, 2, 2, -4, 0, 2},
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, arr := range tests {
+			fraction, _ := New(arr[0], arr[1])
+			other, _ := New(arr[2], arr[3])
+			result, _ := New(arr[4], arr[5])
+			if got := fraction.Add(other); !got.Equal(result) {
+				b.Errorf("Fraction.Add() = %v, want %v", got, result)
+			}
+		}
+	}
+}
+
+func TestFraction_Subtract(t *testing.T) {
+	tests := [][]int64{
+		{1, 2, 3, 4, -1, 4},
+		{3, 7, 4, 7, -1, 7},
+		{4, 9, 3, 7, 1, 63},
+		{1, 2, -2, 4, 1, 1},
+		{1, 2, 2, -4, 2, 2},
+	}
+	for _, arr := range tests {
+		t.Run(fmt.Sprint(arr), func(t *testing.T) {
+			fraction, _ := New(arr[0], arr[1])
+			other, _ := New(arr[2], arr[3])
+			result, _ := New(arr[4], arr[5])
+			if got := fraction.Subtract(other); !got.Equal(result) {
+				t.Errorf("Fraction.Subtract() = %v, want %v", got, result)
+			}
+		})
+	}
+}
+
+func TestFraction_Multiply(t *testing.T) {
+	tests := [][]int64{
+		{1, 2, 3, 4, 3, 8},
+		{3, 7, 4, 7, 12, 49},
+		{4, 9, 3, 7, 4, 21},
+		{1, 2, -2, 4, -1, 4},
+		{1, 2, 2, -4, -1, 4},
+	}
+	for _, arr := range tests {
+		t.Run(fmt.Sprint(arr), func(t *testing.T) {
+			fraction, _ := New(arr[0], arr[1])
+			other, _ := New(arr[2], arr[3])
+			result, _ := New(arr[4], arr[5])
+			if got := fraction.Multiply(other); !got.Equal(result) {
+				t.Errorf("Fraction.Multiply() = %v, want %v", got, result)
+			}
+		})
+	}
+}
+
+func Test_findMaxCommonDividend(t *testing.T) {
+	tests := []struct {
+		i    int64
+		j    int64
+		want int64
+	}{
+		{1, 2, 1},
+		{2, 4, 2},
+		{11, 33, 11},
+		{-2, 6, 2},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.i, tt.j, tt.want), func(t *testing.T) {
+			if got := findMaxCommonDividend(tt.i, tt.j); got != tt.want {
+				t.Errorf("findMaxCommonDividend() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkFindMaxCommonDividend(b *testing.B) {
+	tests := []struct {
+		i    int64
+		j    int64
+		want int64
+	}{
+		{1, 2, 1},
+		{2, 4, 2},
+		{11, 33, 11},
+		{-2, 6, 2},
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			if got := findMaxCommonDividend(tt.i, tt.j); got != tt.want {
+				b.Errorf("findMaxCommonDividend() = %v, want %v", got, tt.want)
+			}
+		}
+	}
+}
+
+func TestFraction_Divide(t *testing.T) {
+	tests := [][]int64{
+		{1, 2, 3, 4, 2, 3},
+		{3, 7, 4, 7, 3, 4},
+		{4, 9, 3, 7, 28, 27},
+		{1, 2, -2, 4, -1, 1},
+		{1, 2, 2, -4, -1, 1},
+	}
+	for _, arr := range tests {
+		t.Run(fmt.Sprint(arr), func(t *testing.T) {
+			fraction, _ := New(arr[0], arr[1])
+			other, _ := New(arr[2], arr[3])
+			result, _ := New(arr[4], arr[5])
+			if got, _ := fraction.Divide(other); !got.Equal(result) {
+				t.Errorf("Fraction.Multiply() = %v, want %v", got, result)
 			}
 		})
 	}
